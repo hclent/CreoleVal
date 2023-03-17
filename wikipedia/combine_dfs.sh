0ae1e29b493a4f3fa476f637190f4958aeb32486
Yiyi Chen <chen.yiyi@protonmail.com>
2023-03-17 11:30:22 +0100

    combine dfs

---
+++ wikipedia/combine_dfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --partition=prioritized
+#SBATCH --job-name=fuzzy
+#SBATCH --output=%j.out
+#SBATCH --time=30:00:00
+#SBATCH --mem=64GB
+
+
+lang=$1
+
+source $HOME/.bashrc
+conda activate creole
+
+cd $HOME/Creole-NLU-NLG-Suite/wikipedia
+
+
+
+python src/RelationExtraction/combine_results.py "$lang"
---
+++ wikipedia/src/RelationExtraction/combine_results.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+
+
+def combine_dfs(lang, folder="data/fuzzywuzzy/results/", outputfolder="data/fuzzywuzzy/df_results"):
+    df = pd.DataFrame()
+    for file in os.listdir(folder):
+        if file.endswith(".csv") and file.startswith(lang):
+
+            lang_, k = file.replace(".csv", "").split("_")
+            assert lang_ == lang
+
+            print(f"loading file {filepath}")
+            filepath = os.path.join(folder, file)
+
+            df_ = pd.read_csv(filepath, index_col=0)
+            if df.empty:
+                df = df_[["text", "text_preprocessed"]]
+
+            assert len(df) == len(df_)
+
+            df[k] = df_["fuzzy_cluster"]
+            del df_
+    df.to_csv(os.path.join(outputfolder, f"{lang}.csv"), index=False)
+
+
+if __name__ == '__main__':
+    import plac
+
+    plac.call(combine_dfs)
