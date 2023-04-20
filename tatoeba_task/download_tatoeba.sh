8dad28bec55a6bdffb833974277a67e1573a0611
Marcell Fekete <37778035+vrmer@users.noreply.github.com>
2023-04-20 11:44:51 +0200

    add deu, fra, nor sets to download and extract

--- tatoeba_task/download_tatoeba.sh
+++ tatoeba_task/download_tatoeba.sh
@@ -11,6 +11,9 @@ https://object.pouta.csc.fi/Tatoeba-Challenge-v2021-08-07/eng-sag.tar
 https://object.pouta.csc.fi/Tatoeba-Challenge-v2021-08-07/eng-tpi.tar
 https://object.pouta.csc.fi/Tatoeba-Challenge-v2021-08-07/fra-hat.tar
 https://object.pouta.csc.fi/Tatoeba-Challenge-v2021-08-07/fra-gcf.tar
+https://github.com/Helsinki-NLP/Tatoeba-Challenge/raw/master/data/devtest/deu-eng/test-v2021-03-10.txt
+https://github.com/Helsinki-NLP/Tatoeba-Challenge/raw/master/data/devtest/eng-fra/test-v2021-07-22.txt
+https://github.com/Helsinki-NLP/Tatoeba-Challenge/raw/master/data/devtest/eng-nor/test-v2020-05-31.txt
 )
 
 # variables for extracting src and trg files
@@ -20,24 +23,43 @@ ARCHIVED="data/release/*/*/*.gz"
 
 mkdir tatoeba
 
-for URL in ${URLS[@]}
+for URL in "${URLS[@]}"
 do
-	LANGTAR=${URL:(-11)}
-	LANG=${LANGTAR:0:7}
-	if
-		[ -d tatoeba/$LANG ]
-	then
-		echo "$LANG already exists"
-	else
-		echo "downloading $LANG"
-		wget $URL
-		tar -xvf $LANGTAR
-		gzip -dv $ARCHIVED
-		mkdir tatoeba/$LANG
-		mv $TARGET tatoeba/$LANG/
-		mv $SOURCE tatoeba/$LANG/
-		rm -r data
-		rm -r $LANGTAR
-	fi
+        if [[ $URL == *.tar  ]]
+        then
+                LANGTAR=${URL:(-11)}
+                LG=${LANGTAR:0:7}
+                if
+                        [ -d "tatoeba/$LG"  ]
+                then
+                        echo "$LG already exists"
+                else
+                        echo "downloading $LG"
+                        wget "$URL"
+                        tar -xvf "$LANGTAR"
+                        gzip -dv "$ARCHIVED"
+                        mdkir "tatoeba/$LG"
+                        mv "$TARGET" "tatoeba/$LG/"
+                        mv "$SOURCE" "tatoeba/$LG/"
+                        rm -r data
+                        rm -r "$LANGTAR"
+                fi
+        else
+                IFS="/" read -ra ARRAY <<< "$URL"
+                LG=${ARRAY[9]}
+                FILENAME=${ARRAY[10]}
+                if
+                        [ -d "tatoeba/$LG" ]
+                then
+                        echo "$LG already exists"
+                else
+                        echo "downloading $LG"
+                        wget "$URL"
+                        mkdir "tatoeba/$LG"
+                        cut -f 3 "$FILENAME" > "tatoeba/$LG/test.src"
+                        cut -f 4 "$FILENAME" > "tatoeba/$LG/test.trg"
+                        rm "$FILENAME"
+                fi
+        fi
 done
 
