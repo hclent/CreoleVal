ec2c90aa47163b39a656541df6c8118d0b4f50aa
Marcell Fekete <37778035+vrmer@users.noreply.github.com>
2023-04-21 13:21:16 +0200

    add code for analyses

--- tatoeba_task/analysis.py
+++ tatoeba_task/analysis.py
@@ -3,23 +3,31 @@ import glob
 import nltk
 import argparse
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from transformers import AutoTokenizer, MT5Tokenizer
 
 
 nltk.download("punkt")
+plt.style.use("seaborn")
+
+BINS = {1: [""],
+        2: ["low", "high"],
+        3: ["low", "moderate", "high"],
+        4: ["very low", "medium low", "medium high", "very high"]
+        }
 
 
 def calculate_factors(df_path, bin_count=4, outpath=None):
     """
-    pass
+    Given a tsv with experimental outputs, it calculates source fertility
+    and subword intersect ratio between source and target sentences.
     """
-    df = pd.read_csv(df_path, sep="\t", index_col=0)
+    if isinstance(df_path, str):
+        df = pd.read_csv(df_path, sep="\t", index_col=0)
+    else:
+        df = df_path
     df.fillna("", inplace=True)
-    # print(df.tail())
-    # exit(1)
     df = df.filter(items=["source_sent", "gold_sent", "correct_pair", "predicted_sent"])
 
     # source sentence length
@@ -36,17 +44,75 @@ def calculate_factors(df_path, bin_count=4, outpath=None):
 
     # intersection
     intersects = []
-
     for src_sw, gold_sw in zip(src_subwords, gold_subwords):
-        src_set, gold_set = set(src_sw), set(gold_sw)
-        matched = src_set.intersection(gold_set)
-        intersect = len(matched)
+        intersect = len([subw for subw in src_sw if subw in gold_sw])
         intersects.append(intersect)
 
     df["subw_intersect_count"] = intersects
     df["subw_intersect_ratio"] = round(df["subw_intersect_count"] / df["src_subw_len"], 3)
 
-    print(df)
+    # unk tokens
+    unks = []
+    for src_sw in src_subwords:
+        unk = len([sw for sw in src_sw if sw in {"[UNK]", "<unk>"}])
+        unks.append(unk)
+
+    df["src_unks"] = unks
+
+    # arrange factors into bins
+    for factor_name in {"src_fertility", "subw_intersect_ratio", "src_unks"}:
+        if factor_name in {"src_unks"}:
+            df[f"binned_{factor_name}"] = pd.qcut(df[factor_name], q=bin_count, duplicates="drop", precision=0)
+        else:
+            df[f"binned_{factor_name}"] = pd.qcut(df[factor_name], q=bin_count, duplicates="drop")
+
+    if outpath:
+        df.to_csv(outpath, sep="\t")
+    return df
+
+
+def return_bin_values(df, factor):
+    """
+    Given a dataframe and a factor,
+    it returns a binned representation of the factor.
+    """
+    bin_dict = defaultdict(dict)
+    binned_factor = f"binned_{factor}"
+
+    df_filtered = df.filter(items=[binned_factor, "correct_pair"])
+    grouped_df = df_filtered.groupby(binned_factor)
+
+    for bin_group, group in grouped_df:
+        correct_count = sum(group["correct_pair"])
+        bin_size = len(group)
+        accuracy = round(correct_count / bin_size * 100, 2)
+        bin_dict[bin_group]["accuracy"] = accuracy
+        bin_dict[bin_group]["size"] = bin_size
+
+    return pd.DataFrame(bin_dict, index=None).T
+
+
+def plot_binned_table(df, factor, output_path, **kwargs):
+    """
+    Provided an input dataframe, the function plots the bins on a bar chart.
+    """
+    bin_names = BINS[len(df)]
+    index_dict = {k: i for k, i in zip(df.index, bin_names)}
+    df.rename(index=index_dict, inplace=True)
+    ax = df.plot.bar(y="accuracy", rot=0, legend=False, **kwargs)
+    # annotate bar chart for the size of the bins
+    for p, s in zip(ax.patches, df["size"]):
+        ax.annotate(str(int(s)),
+                    (p.get_x() + p.get_width() / 2,  # x coordinate
+                     p.get_height()),  # y coordinate
+                    xytext=(0, 0),
+                    textcoords="offset points",
+                    ha="center",
+                    va="bottom")
+    if output_path:
+        # output_path = os.path.join(output_path, f"{factor}.pdf")
+        plt.savefig(output_path)
+    # plt.show()
 
 
 if __name__ == "__main__":
@@ -54,24 +120,83 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Arguments to plot fertility values.")
     parser.add_argument("input_folder", help="path to the folder containing all samples")
     parser.add_argument("-o", "--output_folder", help="path to the folder where the plots should be output")
+    parser.add_argument("-b", "--bin_count", help="count of bins to assign data into")
 
     args = parser.parse_args()
 
     all_samples = os.path.join(args.input_folder, "**/**/**.tsv")
 
+    full_df = None
+
     for tsv in sorted(glob.glob(all_samples)):
 
-        print(tsv)
+        dir_name = os.path.dirname(tsv)
+
+        # print(tsv)
+
+        if dir_name.endswith("sample1"):
+            dfs = []
+            df_to_sample1 = pd.read_csv(tsv, sep="\t", index_col=0)
+            dfs.append(df_to_sample1)
+            continue
+        elif dir_name.endswith("sample2"):
+            df_to_sample2 = pd.read_csv(tsv, sep="\t", index_col=0)
+            dfs.append(df_to_sample2)
+            continue
+        elif dir_name.endswith("sample3"):
+            df_to_sample3 = pd.read_csv(tsv, sep="\t", index_col=0)
+            dfs.append(df_to_sample3)
+            full_df = pd.concat(dfs, ignore_index=True)
 
         lang_pair_full = os.path.dirname(tsv)  # full name of the language pair (including sample name)
         model_name = os.path.basename(os.path.dirname(lang_pair_full))  # model_name
+        lang_pair = os.path.basename(lang_pair_full)
+
+        if (lang_pair.endswith("sample1") or
+                lang_pair.endswith("sample2") or
+                lang_pair.endswith("sample3")):
+            lang_pair = lang_pair[:-8]
 
         if model_name == "mt5-base":
             tokenizer = MT5Tokenizer.from_pretrained("google/mt5-base")
         elif model_name == "random":
-            pass
+            continue
         else:
             tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-        calculate_factors(tsv)
-        break
+        if full_df is not None:
+            factor_df = calculate_factors(full_df)
+        else:
+            factor_df = calculate_factors(tsv)
+
+        lg, eng = lang_pair.split("-")
+
+        for factor in {"src_fertility", "subw_intersect_ratio", "src_unks"}:
+
+            if factor == "subw_intersect_ratio":
+                xlabel = f"Ratio of token overlap ({lg})"
+                factor_name = "subword intersect ratio"
+            elif factor == "src_fertility":
+                xlabel = f"Tokenizer fertility ({lg})"
+                factor_name = "sentence fertility"
+            elif factor == "src_unks":
+                xlabel = f"Unknown token count ({lg})"
+                factor_name = "unknown tokens"
+
+            kwargs = {"xlabel": xlabel,
+                      "ylabel": "Accuracy scores"
+                      }
+
+            bin_df = return_bin_values(factor_df, factor)
+
+            if len(bin_df) > 0:
+
+                output_dir = os.path.join(args.output_folder, factor)
+
+                os.makedirs(output_dir, exist_ok=True)
+
+                output_path = os.path.join(output_dir, f"{lg}.pdf")
+
+                plot_binned_table(bin_df, factor, output_path=output_path, **kwargs)
+
+        full_df = None
