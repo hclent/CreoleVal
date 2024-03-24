fbc0220ef40ad104cc56ed656e84e9922038125c
yiyi chen <chen.yiyi@protonmail.com>
2024-03-24 20:26:41 +0100

    script and model

--- nlu/relation_classification/src/train_zsbert.sh
+++ nlu/relation_classification/src/train_zsbert.sh
@@ -4,16 +4,19 @@
 source /home/cs.aau.dk/ng78zb/miniconda3/etc/profile.d/conda.sh
 conda activate creole
 
-SEED=$1
-TRANSFORMER=$2
-SENTENCE_TRANSFORMER=$3
-BATCH_SIZE=$4
+TRANSFORMER=$1
+SENTENCE_TRANSFORMER=$2
+BATCH_SIZE=$3
 
 
 #
 #model=('bert-base-multilingual-cased' 'xlm-roberta-base' )
 #sentence=('bert-base-nli-mean-tokens' 'bert-large-nli-mean-tokens' 'xlm-r-bert-base-nli-mean-tokens' 'xlm-r-100langs-bert-base-nli-mean-tokens')
+seeds=(2 3 5 7 9)
 
 echo "TRAINING SBERT"
-CUDA_VISIBLE_DEVICES=0 python ZS_BERT/model/train_wiki.py -s ${SEED} -b ${BATCH_SIZE} -t ${TRANSFORMER} -se ${SENTENCE_TRANSFORMER}
 
+for s in "${seeds[@]}"; do
+  echo "seed $s"
+  CUDA_VISIBLE_DEVICES=0 python ZS_BERT/model/train_wiki.py -s ${s} -b ${BATCH_SIZE} -t ${TRANSFORMER} -se ${SENTENCE_TRANSFORMER}
+done
