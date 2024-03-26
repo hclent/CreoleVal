6962fedfe7cebb5f7dcf8ad637bbe438d5e10ee0
Yiyi Chen <chen.yiyi@protonmail.com>
2024-03-26 20:00:55 +0100

    slrum script

--- nlu/relation_classification/src/slurm_train_zsbert.sh
+++ nlu/relation_classification/src/slurm_train_zsbert.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
 #SBATCH --job-name=zsbert
-#SBATCH --output=zsbert_train_%j.out
-#SBATCH --error=zsbert_train_%j.err
+#SBATCH --account=project_465000909
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --mem=50GB
-#SBATCH --time=2-00:00:00
+#SBATCH --output=zs_finetune_%j.out
+#SBATCH --error=zs_finetune_%j.err
 
 set -x
+
+
 
 SEED=$1
 TRANSFORMER=$2
@@ -16,7 +20,10 @@ BATCH_SIZE=$4
 wd=$(pwd)
 echo "working directory ${wd}"
 
-SIF=/home/cs.aau.dk/ng78zb/pytorch_23.10-py3.sif
+# LUMI
+
+SIF=/scratch/project_465000909/lumi-pytorch-rocm-5.6.1-python-3.10-pytorch-v2.1.0-dockerhash-aa8dbea5e0e4.sif
+# SIF=/home/cs.aau.dk/ng78zb/pytorch_23.10-py3.sif
 echo "sif ${SIF}"
 
 
@@ -25,5 +32,12 @@ chmod +x ${wd}/train_zsbert.sh
 
 echo "setting transformer to ${TRANSFORMER} and sentence embedding to ${SENTENCE_TRANSFORMER}"
 echo "batch size ${BATCH_SIZE}, SEED ${SEED}"
-srun singularity exec --nv --cleanenv --bind ${wd}:${wd} \
+
+# aicloud
+#srun singularity exec --nv --cleanenv --bind ${wd}:${wd} \
+#    ${SIF} ${wd}/train_zsbert.sh ${SEED} ${TRANSFORMER} ${SENTENCE_TRANSFORMER} ${BATCH_SIZE}
+
+srun singularity exec \
+    -B /scratch/project_465000909:/scratch/project_465000909 \
+    -B ${wd}:${wd} \
     ${SIF} ${wd}/train_zsbert.sh ${SEED} ${TRANSFORMER} ${SENTENCE_TRANSFORMER} ${BATCH_SIZE}
