77ebb0f9dd67c7fa55afb57f4e36a9fae3a5469e
Yiyi Chen <chen.yiyi@protonmail.com>
2024-03-28 13:01:29 +0100

    read results

---
+++ nlu/relation_classification/src/read_results.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import json
+from collections import defaultdict
+
+transformers = ['bert-base-multilingual-cased', 'xlm-roberta-base']
+encoders = ['bert-base-nli-mean-tokens',
+    'bert-large-nli-mean-tokens',
+    'xlm-r-100langs-bert-base-nli-mean-tokens',
+    'xlm-r-bert-base-nli-mean-tokens']
+
+record_dict = defaultdict(dict)
+for m in transformers:
+    for encoder in encoders:
+        for seed in [564, 757, 991]:
+            filepath = f"output/{m}_{encoder}_{seed}.json"
+            print(f"loading {filepath}")
+            with open(filepath) as f:
+                data = json.load(f)
+            assert data["model"]["seed"] == str(seed)
+            # bi, cbk-zam, jam, tpi
+            record_dict["Dev(en)"][seed] = data["model"]["dev"]
+            for creole in ["bi", "cbk-zam", "jam", "tpi"]:
+                record_dict[creole][seed] = data["creoles"][creole]
+
+df = pd.DataFrame.from_dict(record_dict, orient="index")
+print(df)
