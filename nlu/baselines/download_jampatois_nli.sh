8e3a86457abd4b3fc8bf6255c8f4917b4a1f061d
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2024-04-10 15:25:25 +0200

    Update jampatois download script

--- nlu/baselines/download_jampatois_nli.sh
+++ nlu/baselines/download_jampatois_nli.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Download pretraining MNLI Glue data (as per JamPatois paper)
+wget https://github.com/nyu-mll/GLUE-baselines/raw/master/download_glue_data.py
+python3 download_glue_data.py --data_dir data/glue --tasks MNLI
+
+# Download Fine-tuning Jamaican data
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATADIR="$SCRIPTDIR/data/jam-nli-data"
 FILELIST=(
