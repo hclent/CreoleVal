96fdf44708745b683973db95f3d720b343708d31
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2024-05-02 14:24:18 +0200

    add nli

---
+++ nlu/nli/download_jampatois_nli.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Download pretraining MNLI Glue data (as per JamPatois paper)
+# check if download_glue_data exists, if not then wget it
+if [ ! -f download_glue_data.py ]; then
+    wget https://github.com/nyu-mll/GLUE-baselines/raw/master/download_glue_data.py
+fi
+python3 download_glue_data.py --data_dir data/glue --tasks MNLI
+
+# Download Fine-tuning Jamaican data
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DATADIR="$SCRIPTDIR/data/jam-nli-data"
+FILELIST=(
+    "https://github.com/ruth-ann/jampatoisnli/raw/main/datasets/patoisnli/jampatoisnli-train.csv"
+    "https://github.com/ruth-ann/jampatoisnli/raw/main/datasets/patoisnli/jampatoisnli-val.csv"
+    "https://github.com/ruth-ann/jampatoisnli/raw/main/datasets/patoisnli/jampatoisnli-test.csv"
+)
+
+mkdir -p "$DATADIR"
+for file in ${FILELIST[@]}; do
+    wget -nc --directory-prefix "$DATADIR" "$file"
+done
